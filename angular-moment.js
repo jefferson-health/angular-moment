185b17958b32b854bbd1d1f655e6e9ea36e15e9e
Nate Vecchiarelli <natevecc@gmail.com>
2016-06-13 12:43:57 -0400

    Fixe module loading not working in webpack

--- angular-moment.js
+++ angular-moment.js
@@ -724,11 +724,11 @@
  			}]);
 	}
 
-	if (typeof define === 'function' && define.amd) {
-		define(['angular', 'moment'], angularMoment);
-	} else if (typeof module !== 'undefined' && module && module.exports) {
+	if (typeof module !== 'undefined' && module && module.exports) {
 		angularMoment(require('angular'), require('moment'));
 		module.exports = 'angularMoment';
+	} else if (typeof define === 'function' && define.amd) {
+		define(['angular', 'moment'], angularMoment);
 	} else {
 		angularMoment(angular, (typeof global !== 'undefined' ? global : window).moment);
 	}
